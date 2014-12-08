5231582ac3654fd4bfcf17f9e59d1ca374ff998a
biologyguy <biologyguy@gmail.com>
2014-12-08 16:10:52 -0500

    Allow in-place modification, and handle greek characters

--- utilities/clean_svg.py
+++ utilities/clean_svg.py
@@ -16,16 +16,24 @@ parser = argparse.ArgumentParser(prog='clean_svg', description='Fix SVG files so
 
 parser.add_argument('svg_file', help='Location of the file you want to clean', action='store')
 parser.add_argument('-o', '--outfile', help="Save the new file somewhere, otherwise it's sent to stdout.", action='store', default=False)
+parser.add_argument('-i', '--in_place', help="Over-write original file", action='store_true')
 
 in_args = parser.parse_args()
-
-with open(os.path.abspath(in_args.svg_file), "r") as ifile:
+in_file = os.path.abspath(in_args.svg_file)
+with open(in_file, "r") as ifile:
     content = ifile.read()
 
 content = html.unescape(content)
 content = re.sub(" ?font-family:.*?;", "", content)
+content = re.sub("Œ±", "α", content)
+content = re.sub("Œ≤", "β", content)
+content = re.sub("Œ≥", "γ", content)
 
-if in_args.outfile:
+if in_args.in_place:
+    with open(in_file, "w") as ofile:
+        ofile.write(content)
+
+elif in_args.outfile:
     with open(os.path.abspath(in_args.outfile), "w") as ofile:
         ofile.write(content)
 
