0b51eb46614a686737cf08d23f040f6cbd589100
biologyguy <biologyguy@gmail.com>
2014-12-19 16:43:34 -0500

    Update to include new SeqBuddy functionality
    -Moved file out of workshop, and into Seq_tools directory
    -Significant refactoring and modification.
    -Still needs a lot of work. This is crazy clunky at the moment.

--- seq_tools/search_assembly.py
+++ seq_tools/search_assembly.py
@@ -10,11 +10,16 @@ from multiprocessing import Lock
 from Bio import SearchIO
 from Bio import SeqIO
 from Bio.Seq import Seq
+from seq_tools import blast, SequencePreparer, guess_alphabet, rename
 
 
 class SeqTools():
     def __init__(self, fasta_file, outdir, seq_type=False):
         self.fasta_file = os.path.abspath(fasta_file)
+
+        if guess_alphabet(SequencePreparer(self.fasta_file)) == "prot":
+            sys.exit("Error: Can't run transdecoder on a protein database.")
+
         self.name = "_".join(fasta_file.split("/")[-1].split(".")[:-1])
         self.outdir = os.path.abspath(outdir)
 
@@ -26,19 +31,13 @@ class SeqTools():
         self.blastdb_dir = "%s/blastdbs" % self.outdir
         self.blast_out_dir = "%s/blast_out" % self.outdir
 
-        self.seq_type = seq_type if (seq_type and seq_type in ["prot", "nucl"]) else self._guess_alphabet()
-
         self.transd_run = False
         self.makeblastdb_run = False
         self.blast_run = False
 
         self.lock = Lock()
 
-    def transdecoder(self, save_tmp=False, force=False):
-        if self.seq_type == "prot":
-            print("Can't run transdecoder on a protein database.")
-            return False
-
+    def transdecoder(self, save_tmp=False, _force=False):
         self.transd_run = True
         if os.path.exists(self.trans_d_dir):
             check_extensions = []
@@ -46,12 +45,12 @@ class SeqTools():
             for next_file in files:
                 check_extensions.append(next_file.split(".")[-1])
 
-            if check_extensions == ["bed", "cds", "gff3", "mRNA", "pep"] and not force:
+            if check_extensions == ["bed", "cds", "gff3", "mRNA", "pep"] and not _force:
                 print("Transdecoder output detected for %s, using previous data. (You can override by force)" % self.name)
                 return False
 
         cwd = os.getcwd()
-        os.chdir(self.temp_dir.dir)
+        os.chdir(self.temp_dir.path)
 
         if not os.path.exists("%s/transdecoder" % self.outdir):
             print("Creating %s/transdecoder" % self.outdir)
@@ -68,8 +67,15 @@ class SeqTools():
         print(dirs)
         for next_file in files:
             if next_file.split(".")[0] == self.name:
-                new_name = "%s_transD.%s" % (self.name, next_file.split(".")[-1])
+                extension = next_file.split(".")[-1]
+                new_name = "%s_transD.%s" % (self.name, extension)
                 shutil.move(next_file, "%s/%s" % (self.trans_d_dir, new_name))
+                # Clean up the ids of the output files
+                if extension in ["cds", "pep", "mRNA"]:
+                    out_seqs = SequencePreparer("%s/%s" % (self.trans_d_dir, new_name))
+                    rename(out_seqs, ".*\|m\.", "m")
+                    with open("%s/%s" % (self.trans_d_dir, new_name), "w") as ofile:
+                        SeqIO.write(out_seqs, ofile, "fasta")
 
         if save_tmp:
             temp_files = dirs[0]
@@ -84,24 +90,38 @@ class SeqTools():
         os.chdir(cwd)
         return True
 
-    def makeblastdb(self, force=False):
+    def makeblastdb(self, _force=False):
         self.makeblastdb_run = True
 
         if not os.path.exists(self.blastdb_dir):
             os.mkdir(self.blastdb_dir)
 
-        if not os.path.exists("%s/%s_prot.phr" % (self.blastdb_dir, self.name)) or force:
+        # Make both prot and nucl databases
+        if not os.path.exists("%s/%s_prot.phr" % (self.blastdb_dir, self.name)) or _force:
             print("Running makeblastdb on %s" % self.name)
-            print("makeblastdb -in %s -dbtype %s -out %s/%s_%s\n" % (self.fasta_file, self.seq_type, self.blastdb_dir, self.name, self.seq_type))
-            Popen("makeblastdb -in %s -dbtype %s -out %s/%s_%s" % (self.fasta_file, self.seq_type, self.blastdb_dir, self.name, self.seq_type), shell=True).wait()
+            print("makeblastdb -in %s/%s_transD.pep -dbtype prot -out %s/%s_prot -parse_seqids\n" %
+                  (self.trans_d_dir, self.name, self.blastdb_dir, self.name))
+            Popen("makeblastdb -in %s/%s_transD.pep -dbtype prot -out %s/%s_prot -parse_seqids" %
+                  (self.trans_d_dir, self.name, self.blastdb_dir, self.name), shell=True).wait()
 
         else:
-            print("Blast db detected for %s, using previous data. (You can override by force)" % self.name)
+            print("Protein blast db detected for %s, using previous data. (You can override with -f)" % self.name)
+            return False
+
+        if not os.path.exists("%s/%s_prot.nhr" % (self.blastdb_dir, self.name)) or _force:
+            print("Running makeblastdb on %s" % self.name)
+            print("makeblastdb -in %s/%s_transD.cds -dbtype nucl -out %s/%s_nucl -parse_seqids\n" %
+                  (self.trans_d_dir, self.name, self.blastdb_dir, self.name))
+            Popen("makeblastdb -in %s/%s_transD.cds -dbtype nucl -out %s/%s_nucl -parse_seqids" %
+                  (self.trans_d_dir, self.name, self.blastdb_dir, self.name), shell=True).wait()
+
+        else:
+            print("CDS blast db detected for %s, using previous data. (You can override with -f)" % self.name)
             return False
 
         return True
 
-    def blast(self, query_file, force=False, keep_blast=False, evalue=0.0001):
+    def blast(self, query_file, _force=False, evalue=0.01):
         if not os.path.exists(query_file):
             sys.exit("Error: The indicated query file does not exist. I need some sequences to search with.")
 
@@ -114,48 +134,28 @@ class SeqTools():
         if not os.path.exists(self.blast_out_dir):
             os.mkdir(self.blast_out_dir)
 
-        blast_program = "blastp" if self.seq_type == "prot" else "blastn"
-        if not os.path.exists("%s/%s|%s_query.txt" % (self.blast_out_dir, self.name, query_name)) or force:
-            print("Running %s with %s on %s" % (blast_program, query_name, self.name))
-            print("%s -db %s/%s_%s -query %s -out '%s/%s|%s_query.txt' -num_threads 20 -evalue %s -outfmt 6\n" % (blast_program, self.blastdb_dir, self.name, self.seq_type, query_file, self.blast_out_dir, self.name, query_name, evalue))
-            Popen("%s -db %s/%s_%s -query %s -out '%s/%s|%s_query.txt' -num_threads 20 -evalue %s -outfmt 6" % (blast_program, self.blastdb_dir, self.name, self.seq_type, query_file, self.blast_out_dir, self.name, query_name, evalue), shell=True).wait()
+        query_seqs = SequencePreparer(query_file)
+        seq_type = guess_alphabet(query_seqs)
 
-        else:
-            print("%s output file detected for %s and %s, using previous data. (You can override by force)\n" % (blast_program, self.name, query_name))
-            return False
+        blast_hits = blast(query_seqs, "%s/%s_%s" % (self.blastdb_dir, self.name, seq_type))
 
-        print("Collecting blast hits into FASTA format\n%s\n" % "%s/%s|%s_query.fa" % (self.blast_out_dir, self.name, query_name))
-        records = self._get_records(query_name)
-
-        hit_ids = []
-        for record in records:
-            for hsp in record.hsps:
-                hit_id = hsp.hit_id
-
-                if hit_id in hit_ids:
-                    continue
-
-                hit_ids.append(hit_id)
-
-        print("Collecting match records:")
         with open("%s/%s|%s_query.fa" % (self.blast_out_dir, self.name, query_name), "w") as ofile:
-                ofile.write("")
-
-        run_multicore_function(hit_ids, self._parse_hits, [query_name])
+            for seq in blast_hits.seqs:
+                SeqIO.write(seq, ofile, "fasta")
 
         self.blast_run = True
         return True
 
     def _parse_hits(self, hit_id, args):
         query_name = args[0]
-        seq = Popen("pull_fasta_rec %s '%s'" % (self.fasta_file, hit_id),
+        seq = Popen("pull_fasta_rec %s/%s_transD.cds '%s'" % (self.trans_d_dir, self.name, hit_id),
                     shell=True, stdout=PIPE).communicate()[0]
 
         temp_file = TempFile()
-        with open(temp_file.file, "w") as ofile:
+        with open(temp_file.path, "w") as ofile:
             ofile.write(seq.decode("utf-8"))
 
-        with open(temp_file.file, "r") as ifile:
+        with open(temp_file.path, "r") as ifile:
             seq = SeqIO.read(ifile, 'fasta')
 
         with self.lock:
@@ -187,16 +187,28 @@ class SeqTools():
 if __name__ == '__main__':
     import argparse
 
-    parser = argparse.ArgumentParser(prog="sequence_tools", description="Wrapper tools for Transdecoder and some Blast++ stuff")
+    parser = argparse.ArgumentParser(prog="sequence_tools",
+                                     description="Wrapper tools for Transdecoder and some Blast++ stuff")
 
     parser.add_argument('input_file', help='FASTA file of DNA or Protein sequences')
 
-    parser.add_argument('-o', '--out_dir', help='Specify the directory where you want your output files. Default is current working dir.', action='store', default=os.getcwd())
-    parser.add_argument('-t', '--seq_type', help='Specify whether sequences are protein or dna, otherwise the program will guess.', type=str, choices=["prot", "nucl"], default=False)
-    parser.add_argument('-d', '--transdecoder', help='Run transdecoder on input sequences.', action='store_true', default=False)
-    parser.add_argument('-m', '--makeblastdb', help='Run makeblastdb on input sequences, or transdecoder file if that was run first.', action='store_true', default=False)
-    parser.add_argument('-b', '--blast', help="Run blastp or blastn, and needs an input query file (defaults to blastp if transdecoder has been run, but can be over-ridden by specifying 'prot' or 'nucl' with --seq_type)", action='store')
-    parser.add_argument('-f', '--force', nargs='+', choices=["transD", "blast", "makeblastdb"], help="Indicate if you want previous runs to be over-written", default=[])
+    parser.add_argument('-o', '--out_dir',
+                        help='Specify the directory where you want your output files. Default is current working dir.',
+                        action='store', default=os.getcwd())
+    parser.add_argument('-t', '--seq_type',
+                        help='Specify whether sequences are protein or dna, otherwise the program will guess.',
+                        type=str, choices=["prot", "nucl"], default=False)
+    parser.add_argument('-d', '--transdecoder', help='Run transdecoder on input sequences.', action='store_true',
+                        default=False)
+    parser.add_argument('-m', '--makeblastdb',
+                        help='Run makeblastdb on input sequences, or transdecoder file if that was run first.',
+                        action='store_true', default=False)
+    parser.add_argument('-b', '--blast',
+                        help="Run blastp or blastn, and needs an input query file (defaults to blastp if transdecoder "
+                             "has been run, but can be over-ridden by specifying 'prot' or 'nucl' with --seq_type)",
+                        action='store')
+    parser.add_argument('-f', '--force', nargs='+', choices=["transD", "blast", "makeblastdb"],
+                        help="Indicate if you want previous runs to be over-written", default=[])
 
     in_args = parser.parse_args()
 
@@ -206,22 +218,13 @@ if __name__ == '__main__':
 
     if in_args.transdecoder:
         force = True if ("transD" in in_args.force) else False
-        transome.transdecoder(force=force)
+        transome.transdecoder(_force=force)
 
     if in_args.makeblastdb:
         force = True if ("makeblastdb" in in_args.force) else False
-        transome.makeblastdb(force=force)
+        transome.makeblastdb(_force=force)
 
     if in_args.blast:
         force = True if ("blast" in in_args.force) else False
-        ###first check to see if query file has multiple headers
-        query_headers=[]#build a list
-        for s_record in SeqIO.parse(in_args.blast,"fasta"): #loop through SeqIO generator
-            #if A in list, blow it up
-            if s_record.id in query_headers :
-                #shut down with sys.exit()
-                sys.exit("Shutting program down due to multiple headers in the query file with the same name:\n"+ s_record.id)
-            else:
-                query_headers.append(s_record.id)
-        transome.blast(os.path.abspath(in_args.blast), force=force)
+        transome.blast(os.path.abspath(in_args.blast), _force=force)
 
