a4f34bea08fc0659040dca3e09cad73b8053508a
Steve Bond <biologyguy@gmail.com>
2015-03-08 22:20:15 -0400

    Finish NASDAQ scraper

--- workshop/nasdaq.py
+++ workshop/nasdaq.py
@@ -3,13 +3,13 @@
 # Created on: Mar 4 2015 
 
 """
-DESCRIPTION OF PROGRAM
+Selection criteria:
+- Market cap of > $250M
+- Earnings report more than 2 weeks early
 """
 
 import sys
-import os
 import re
-import shutil
 import requests
 from bs4 import BeautifulSoup
 import time
@@ -22,42 +22,65 @@ def timestamp(date_string):
     new_datetime = datetime.datetime.strptime(date_string, "%m/%d/%Y").timetuple()
     return round(time.mktime(new_datetime))
 
-if __name__ == '__main__':
-    #url = "http://www.nasdaq.com/earnings/earnings-calendar.aspx"
-    #content = requests.get(url).text
-    with open("test_files/nasdaq.html", "r") as ifile:
-        content = ifile.read()
 
-    soup = BeautifulSoup(content, 'html5lib')
-    #paginate = soup.find('div', {"class": 'paginate'}).find_all('a')
+def convert_value(value):  # input as string --> eg $12.43M
+    magnitude = value[-1]
+    value = float(value[1:-1])
+    if magnitude == "M":
+        value *= 1000000
+    elif magnitude == "B":
+        value *= 1000000000
+    elif magnitude == "T":
+        value *= 1000000000000
+    else:
+        sys.exit(magnitude)
+    return value
 
-    week = soup.find(id="two_column_main_content_lreportweek").find_all('a')
-    for day in week:
-        print(day.getText())
-    sys.exit()
 
-    tablerows = soup.find(id='ECCompaniesTable').find_all('tr')
-
-    comp_dict = {}
-    print("#Ticker --\tMarket_cap --\tReport_date --\tLast_year's_report --\tExpected_EPS --\tLast_year's_EPS")
+def company_info(tablerows):
     for row in tablerows[1:]:
         output = ""
         columns = row.find_all("td")
 
-        company_info = columns[1].a.getText()
-        ticker = re.search("\((.*)\)", company_info).group(1)
-        market_cap = re.search("\$[0-9]*\.*[0-9]*[A-Z]*|n/a", company_info).group(0)
+        _company_info = columns[1].a.getText()
+        ticker = re.search("\((.*)\)", _company_info).group(1)
+        market_cap = re.search("\$[0-9]*\.*[0-9]*[A-Z]*|n/a", _company_info).group(0)
+        if market_cap == "n/a" or convert_value(market_cap) < 250000000.:
+            continue
         report_date = columns[2].getText().strip()
         last_years_report_date = columns[6].getText().strip()
         expected_eps = columns[4].getText().strip()
         last_years_eps = columns[7].getText().strip()
 
-        if timestamp(last_years_report_date) and timestamp(report_date) - timestamp(last_years_report_date) - 31536000 + 1209600 < 0:
-            output += "%s\t\t\t" % ticker
-            output += "%s\t\t\t" % market_cap
-            output += "%s\t\t" % report_date
-            output += "%s\t\t\t\t" % last_years_report_date
-            output += "%s\t\t\t" % expected_eps
+        if timestamp(last_years_report_date) \
+                and timestamp(report_date) - timestamp(last_years_report_date) - 31536000 + 1209600 < 0:
+            output += "%s\t" % ticker
+            output += "%s\t" % market_cap
+            output += "%s\t" % report_date
+            output += "%s\t" % last_years_report_date
+            change = ((timestamp(report_date) - timestamp(last_years_report_date) - 31536000) / -86400)
+            output += "%s days\t" % int(change)
+            output += "%s\t" % expected_eps
             output += "%s" % last_years_eps
 
             print(output)
+
+
+if __name__ == '__main__':
+    date = datetime.date.today()
+    today = date.strftime("%d")
+    valid_days = []
+    for _ in range(31):
+        if date.strftime("%a") not in ["Sat", "Sun"]:
+            valid_days.append(date.strftime("%Y-%b-%d"))
+        date += datetime.timedelta(days=1)
+        if date.strftime("%d") == today:
+            break
+
+    print("#Ticker\tMarket cap\tReport date\tLast year's report\tChange\tExpected EPS\tLast year's EPS")
+    for next_day in valid_days:
+        url = "http://www.nasdaq.com/earnings/earnings-calendar.aspx?date=%s" % next_day
+        content = requests.get(url).text
+        soup = BeautifulSoup(content, 'html5lib')
+        if soup.find(id='ECCompaniesTable'):
+            company_info(soup.find(id='ECCompaniesTable').find_all('tr'))
