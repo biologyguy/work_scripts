dc9900afbd5220789963fc3e1fb7120737b72a95
biologyguy <biologyguy@gmail.com>
2015-04-08 11:55:28 -0400

    Structural changes
    -Add hashbang and explicit encoding headers
    -Rename class Test() to class IfThan() (more meaningful name, and does not cause confusion with python unit testing)
    -Add __init__() function for object instantiation.
    -Add __name__ == '__main__' block for proper script execution from command line

--- workshop/ifs_and_thens.py
+++ workshop/ifs_and_thens.py
@@ -1,23 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 __author__ = 'karl'
 import re
 
 
-class Test:
-    patterns = ["if", "then"]
-    strings = ["if it is cold then I will wear a coat", "if I am hungry then I will eat a snack",
-               "if it is dark then I will bring a flashlight"]
-    ifs = []
-    thens = []
+class IfThan():
+    def __init__(self):
+        patterns = ["if", "then"]
+        strings = ["if it is cold then I will wear a coat", "if I am hungry then I will eat a snack",
+                   "if it is dark then I will bring a flashlight"]
+        ifs = []
+        thens = []
 
-    for sentence in strings:
-        match_if = re.search(patterns[0], sentence)
-        match_then = re.search(patterns[1], sentence)
-        start_if = match_if.end() + 1
-        end_if = match_then.start() - 1
-        ifs.append(sentence[start_if:end_if])
-        start_then = match_then.end() + 1
-        end_then = len(sentence)
-        thens.append(sentence[start_then:end_then])
-    print("Ifs\t\t\t|Thens")
-    for x in range(0, len(ifs)):
-        print("%s\t|%s" % (ifs[x], thens[x]))
+        for sentence in strings:
+            match_if = re.search(patterns[0], sentence)
+            match_then = re.search(patterns[1], sentence)
+            start_if = match_if.end() + 1
+            end_if = match_then.start() - 1
+            ifs.append(sentence[start_if:end_if])
+            start_then = match_then.end() + 1
+            end_then = len(sentence)
+            thens.append(sentence[start_then:end_then])
+        print("Ifs\t\t\t|Thens")
+        for x in range(0, len(ifs)):
+            print("%s\t|%s" % (ifs[x], thens[x]))
+
+if __name__ == "__main__":
+    test_obj = IfThan()
