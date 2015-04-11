dd3dd9013c142f04040b34c86bca77bb333b902f
biologyguy <biologyguy@gmail.com>
2015-04-11 13:16:38 -0400

    Process top files and create MSA

--- ancestral_recon/AlignMe/Alignme_run.py
+++ ancestral_recon/AlignMe/Alignme_run.py
@@ -11,7 +11,7 @@ import os
 import re
 import shutil
 from MyFuncs import run_multicore_function, TempDir
-from SeqBuddy import SeqBuddy
+from SeqBuddy import SeqBuddy, delete_metadata
 from pssm import PSSM
 import argparse
 from multiprocessing import Lock
@@ -24,10 +24,10 @@ def clean_up(path_list):
         path_list = [path_list]
     for _next in path_list:
         if os.path.isdir(_next):
-            print("isdir: %s" % _next)
+            #print("isdir: %s" % _next)
             shutil.rmtree(_next)
         elif os.path.isfile(_next):
-            print("isfile: %s" % _next)
+            #print("isfile: %s" % _next)
             os.remove(_next)
     return
 
@@ -57,11 +57,10 @@ def octopus(seq_obj, args):
 
     return
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog="Alignme_run", description="", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("input_file", help="Select the sequence file you want to analyze", action="store")
+    parser.add_argument("input_sequences", help="Select the sequence file(s) you want to analyze", nargs="+", action="store")
     parser.add_argument("-p", "--pssm", action="store",
                         help="Sepecify a PSSM file if pre-calculated. Otherwise it is calculate from the data.")
     parser.add_argument("-b", "--base_name", action="store", help="Specify the prefix for output.")
@@ -70,10 +69,19 @@ if __name__ == '__main__':
     in_args = parser.parse_args()
 
     temporary_directory = TempDir()
-    seqbuddy = SeqBuddy(in_args.input_file)
+
+    seqbuddy = []
+    seq_set = ""
+
+    for seq_set in in_args.input_sequences:
+        seq_set = SeqBuddy(seq_set)
+        seqbuddy += seq_set.records
+
+    seqbuddy = SeqBuddy(seqbuddy)
+    seqbuddy = delete_metadata(seqbuddy)
 
     if not in_args.base_name:
-        base_name = ".".join(in_args.input_file.split("/")[-1].split(".")[:-1])
+        base_name = "AlineMe_input"
     else:
         base_name = in_args.base_name
 
@@ -96,20 +104,40 @@ if __name__ == '__main__':
     
     # Output files
     top_file = "%s/%s_top.fasta" % (out_dir, base_name)
+    seq_file = "%s/%s.fasta" % (out_dir, base_name)
+    with open(seq_file, "w") as ofile:
+        SeqIO.write(seqbuddy.records, ofile, "fasta")
 
     # unlink top and trim files if they already exist...
-    # The calling function opens file in append mode "a", so important to start with empty file
-    clean_up(top_file)
-    
+    # These files are opened in append mode "a", so important to start with empty file
+    clean_up([top_file])
+    top_file_handle = open(top_file, "a")
+
     # multicore stuff
     stdout_lock = Lock()
-    top_file_lock = Lock()
     
     # Run octopus
     print("Execute Octopus on %s")
     run_multicore_function(seqbuddy.records, octopus, ["/usr/local/blastdbs/species_protein/Hydra_magnipapillata",
                                                        temporary_directory])
 
-    print(temporary_directory.subdirs)
-    # clean_up(["%s/PSSM_PRF_FILES" % out_dir, "%s/RAW_PRF_FILES" % out_dir, "%s/exec_times-bloctopus.txt" % out_dir,
-    #          "%s/exec_times-octopus.txt" % out_dir])
+    clean_up(["%s/PSSM_PRF_FILES" % out_dir, "%s/RAW_PRF_FILES" % out_dir, "%s/exec_times-bloctopus.txt" % out_dir,
+              "%s/exec_times-octopus.txt" % out_dir])
+
+    # process top files
+    print("\nProcess top files")
+    for rec in seqbuddy.records:
+        top_in_file = "%s/%s.top" % (out_dir, rec.id)
+        with open(top_in_file, "r") as ifile:
+            top_file_handle.write(ifile.read())
+        clean_up([top_in_file])
+
+    top_file_handle.close()
+    print("  ---> DONE")
+
+    # create MSAs with MAFFT
+    mafft_command = "einsi --thread -1 --quiet %s > %s/%s_einsi.fasta" % (seq_file, out_dir, base_name)
+    print("\nMAFFT alignment\n%s" % mafft_command)
+    Popen(mafft_command, shell=True).wait()
+    print("  --> DONE")
+
