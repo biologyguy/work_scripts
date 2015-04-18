56fcd59403a375d9d72877fee5bd5f92fea6059f
biologyguy <biologyguy@gmail.com>
2015-04-18 11:08:59 -0400

    Overhaul
    - Change the order of chdir() movement and how the temp_dir is managed
    - Add a --dependencies flag, and remove the original behavior where everything in the main script's directory was bundled
    - Bottom line: The script and all dependencies can now be pulled together from disparate places without needing to shuffle them around

--- utilities/compile_python.py
+++ utilities/compile_python.py
@@ -12,32 +12,42 @@ import argparse
 import MyFuncs
 from subprocess import Popen
 
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog="compile_python", description="",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument("main_file", action="store",
-                        help="Specify the main script to be executed. All files and directories in the same directory "
-                             "as that file will be included in the final executable.")
+                        help="Specify the main script to be executed.")
+    parser.add_argument("-d", "--dependencies", nargs="+",
+                        help="List of directories and files to be bundled with script.")
 
     in_args = parser.parse_args()
 
-    temp_dir = MyFuncs.TempDir()
-    current_dir = os.getcwd()
-    main_dir = os.path.abspath(in_args.main_file).split("/")
-    script_name = str(main_dir[-1])
-    main_dir = "/".join(main_dir[:-1])
-    shutil.copytree(main_dir, "%s/temp" % temp_dir.path)
-    os.chdir("%s/temp" % temp_dir.path)
-    os.rename(script_name, "__main__.py")
+    main_file = os.path.abspath(in_args.main_file)
+    if in_args.dependencies:
+        in_args.dependencies = [os.path.abspath(dpndy) for dpndy in in_args.dependencies]
 
-    Popen("zip -r ../new_app.zip *", shell=True).wait()
-    os.chdir(temp_dir.path)
+    temp_dir = MyFuncs.TempDir()
+    os.mkdir("%s/temp" % temp_dir.path)
+    current_dir = os.getcwd()
+    os.chdir("%s/temp" % temp_dir.path)
+
+    script_name = str(main_file.split("/")[-1])
     script_name = ".".join(script_name.split(".")[:-1])
+    shutil.copyfile(main_file,  "./__main__.py")
+
+    if in_args.dependencies:
+        for dpndy in in_args.dependencies:
+            if os.path.isfile(dpndy):
+                shutil.copyfile(dpndy, "./%s" % dpndy.split("/")[-1])
+            else:
+                shutil.copytree(dpndy,  "./%s" % dpndy.split("/")[-1])
+
+    Popen("zip -rq ../new_app.zip *", shell=True).wait()
+    os.chdir("../")
     Popen("echo '#!/usr/bin/env python3' | cat - new_app.zip > %s" % script_name, shell=True).wait()
     Popen("chmod +x %s" % script_name, shell=True).wait()
-    Popen("mv %s %s/%s" % (script_name, main_dir, script_name), shell=True).wait()
     os.chdir(current_dir)
+    Popen("mv %s/%s ./" % (temp_dir.path, script_name), shell=True).wait()
 
     print('New executable created: %s' % script_name)
