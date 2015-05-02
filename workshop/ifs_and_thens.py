24db08c47a240a0c4710d811d8753ac67f22ad15
karl <karlkeat@gmail.com>
2015-05-01 21:23:06 -0400

    Added support for reading text files from a URL. Currently does not print anything for some reason.

--- workshop/ifs_and_thens.py
+++ workshop/ifs_and_thens.py
@@ -3,28 +3,36 @@
 
 __author__ = 'karl'
 import re
+import urllib.request
 
 
 class IfThan():
     def __init__(self):
-        patterns = ["if", "then"]
-        strings = ["if it is cold then I will wear a coat", "if I am hungry then I will eat a snack",
-                   "if it is dark then I will bring a flashlight"]
-        ifs = []
-        thens = []
+        book_url = input("Please input a book url:")
+        book_text = urllib.request.urlopen(book_url).read()
+        book_text = book_text.decode("utf-8").split(".")
+        output = self.parse_book_text(book_text)
+        length = len(output[0])
+        for x in range(0, length):
+            print(output[0][x] + " " + output[1][x])
 
+    def parse_book_text(self, strings):
+        patterns = ["if", "then"]
+        output = []
+        output.append([])
+        output.append([])
         for sentence in strings:
             match_if = re.search(patterns[0], sentence)
             match_then = re.search(patterns[1], sentence)
-            start_if = match_if.end() + 1
-            end_if = match_then.start() - 1
-            ifs.append(sentence[start_if:end_if])
-            start_then = match_then.end() + 1
-            end_then = len(sentence)
-            thens.append(sentence[start_then:end_then])
-        print("Ifs\t\t\t|Thens")
-        for x in range(0, len(ifs)):
-            print("%s\t|%s" % (ifs[x], thens[x]))
+            if not (match_if == None or match_then == None):
+                start_if = match_if.end() + 1
+                end_if = match_then.start() - 1
+                output[0].append(sentence[start_if:end_if])
+                start_then = match_then.end() + 1
+                end_then = len(sentence)
+                output[1].append(sentence[start_then:end_then])
+        return output
+
 
 if __name__ == "__main__":
     test_obj = IfThan()
