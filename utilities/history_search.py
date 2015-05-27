b0397eb60761e645e581be1f5a82e425bf201e72
biologyguy <biologyguy@gmail.com>
2015-05-27 14:11:23 -0400

    Add new flag to restrict output to specific PID

--- utilities/history_search.py
+++ utilities/history_search.py
@@ -18,6 +18,7 @@ if __name__ == '__main__':
     parser.add_argument("depth", help="Number of matches to return", action="store", nargs='?', type=int, default=10)
     parser.add_argument("-d", "--date", help="Specify a specific history file", action="store")
     parser.add_argument("-ld", "--list_dates", help="List the available history files", action="store_true")
+    parser.add_argument("-pid", "--process_id", help="Restrict results to specific pid.", action="store")
 
     in_args = parser.parse_args()
 
@@ -44,7 +45,12 @@ if __name__ == '__main__':
     output = []
     while in_args.depth > 0 and len(history_list) > 0:
         line = history_list.pop()
+        if in_args.process_id:
+            if not re.search(":%s " % in_args.process_id, line):
+                continue
+
         command = re.sub(":[0-9]* [JFMASOND][a-z]{2}/[0-3][0-9]/[0-9]{2} [0-9]{2}:[0-9]{2}; ", "", line)
+
         if re.search(in_args.regex, command):
             output.append(line)
             in_args.depth -= 1
