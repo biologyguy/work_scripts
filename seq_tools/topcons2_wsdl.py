78f9297683c6ca54621a59e1285ded4f06916be4
biologyguy <biologyguy@gmail.com>
2016-04-07 11:48:34 -0400

    Complete refactor of input handling
    - Use argparse to handle arguments
    - Process input files with SeqBuddy

--- seq_tools/topcons2_wsdl.py
+++ seq_tools/topcons2_wsdl.py
@@ -5,8 +5,8 @@
 
 import os
 import sys
-progname = os.path.basename(sys.argv[0])
-wspace = ''.join([" "] * len(progname))
+import SeqBuddy as Sb
+import buddy_resources as br
 
 try:
     from suds.client import Client
@@ -16,183 +16,31 @@ except ImportError:
 
 import urllib.request
 
-MAX_FILESIZE_IN_MB = 9
-MAX_FILESIZE = MAX_FILESIZE_IN_MB * 1024 * 1024
 
-usage_short = """
-Usage: %s -m submit|get [-seq SEQFILE] [-jobname NAME] [-email EMAIL]
-       %s               [-fix FIXFILE]
-       %s               [-jobid JOBID] [-outpath DIR]
-""" % (progname, wspace, wspace)
-
-usage_ext="""
-Description:
-    Access topcons2 web-server (http://v2.topcons.net) through WSDL service
-
-OPTIONS:
-  -m submit|get  Set the mode
-                 submit - submit a job to WSDL
-                 get    - retrieve the result from the server
-
-  -seq    FILE   Supply input sequence in FASTA format
-
-  -jobname STR   Give the job a name
-
-  -email   STR   Send a notification to the email when the result is ready
-
-  -jobid   STR   Retrieve the result by supplying a valid jobid
-
-  -outpath DIR   Save the retrieved data to outpath, (default: ./)
-
-  -fix    FILE   Supply a file with topology constraints (TO BE IMPLEMENTED)
-
-  -h, --help     Print this help message and exit
-
-Created 2015-02-04, updated 2015-02-06, Nanjiang Shu
-"""
-
-usage_exp = """
-Examples:
-    # submit test.fa with jobname 'test' to the server
-    %s -m submit -seq test.fa -jobname test
-
-    # try to retrieve the result for jobid 'rst_TTT' and save it to the current
-    # directory
-    %s -m get -jobid rst_TTT
-
-""" % (progname, progname)
-
-
-def my_getopt_str(argv, i):
-    """
-    Get a string from the argument list, return the string and the updated
-    index to the argument list
-    """
-    try:
-        opt = argv[i + 1]
-        if opt[0] == "-":
-            msg = "Error! option '%s' must be followed by a string, not an option arg."
-            print(msg % argv[i], file=sys.stderr)
-            sys.exit(1)
-        return opt, i + 2
-    except IndexError:
-        msg = "Error! option '%s' must be followed by a string"
-        print(msg % argv[i], file=sys.stderr)
-        sys.exit(1)
-
-
-def print_help(fpout=sys.stdout):
-    print(usage_short, file=fpout)
-    print(usage_ext, file=fpout)
-    print(usage_exp, file=fpout)
-
-
-def read_file(infile, mode="r"):
-    try:
-        fpin = open(infile, mode)
-        content = fpin.read()
-        fpin.close()
-        return content
-    except IOError:
-        print("Failed to read file %s with mode '%s'" % (infile, mode), file=sys.stderr)
-        return ""
-
-
-def main(g_params):
-    argv = sys.argv
-    num_argv = len(argv)
-    if num_argv < 2:
-        print_help()
-        return 1
-
+def main(in_args):
     wsdl_url = "http://v2.topcons.net/pred/api_submitseq/?wsdl"
-    mode = ""
-    jobid = ""
-    email = ""
-    jobname = ""
-    fixtopfile = ""
-    seqfile = ""
-    outpath = "./"
+    fixtop = ""
 
-    i = 1
-    is_non_option_arg = False
-    while i < num_argv:
-        if is_non_option_arg:
-            print("Error! Wrong argument: %s" % argv[i], file=sys.stderr)
-            return 1
-
-        elif argv[i] == "--":
-            is_non_option_arg = True
-            i += 1
-
-        elif argv[i][0] == "-":
-            if argv[i] in ["-h", "--help"]:
-                print_help()
-                return 0
-            elif argv[i] in ["-m", "--m"]:
-                (mode, i) = my_getopt_str(argv, i)
-            elif argv[i] in ["-seq", "--seq"]:
-                (seqfile, i) = my_getopt_str(argv, i)
-            elif argv[i] in ["-jobname", "--jobname"]:
-                (jobname, i) = my_getopt_str(argv, i)
-            elif argv[i] in ["-email", "--email"]:
-                (email, i) = my_getopt_str(argv, i)
-            elif argv[i] in ["-fix", "--fix"]:
-                (fixtopfile, i) = my_getopt_str(argv, i)
-            elif argv[i] in ["-jobid", "--jobid"]:
-                (jobid, i) = my_getopt_str(argv, i)
-            elif argv[i] in ["-outpath", "--outpath"]:
-                (outpath, i) = my_getopt_str(argv, i)
-            else:
-                print("Error! Wrong argument: %s" % argv[i], file=sys.stderr)
-                return 1
-        else:
-            print("Error! Wrong argument: %s" % argv[i], file=sys.stderr)
-            return 1
-
-    if mode == "":
-        print("mode not set. exit!", file=sys.stderr)
-        print(usage_short)
-        return 1
-    elif mode not in ["submit", "get"]:
-        print("unrecognized mode. exit!", file=sys.stderr)
-        print(usage_short)
-        return 1
-
-    if mode == "submit":
-        if seqfile == "":
-            print("You want to submit a job but seqfile not set. exit!", file=sys.stderr)
-            print(usage_short)
-            return 1
-        elif not os.path.exists(seqfile):
-            print("seqfile %s does not exist. exit!" % seqfile, file=sys.stderr)
-            return 1
-
+    if os.path.isfile(in_args.input):
         try:
-            filesize = os.path.getsize(seqfile)
-        except OSError:
-            print("failed to get the size of seqfile %s. exit" % seqfile, file=sys.stderr)
+            seqbuddy = Sb.SeqBuddy(in_args.input, out_format="fasta")
+            Sb.clean_seq(seqbuddy)
+            # Sb.hash_ids(seqbuddy)
+
+        except br.GuessError:
+            print("Unable to read the provided input file, is it a properly formatted sequence file?")
             return 1
 
-        if filesize >= MAX_FILESIZE:
-            print("You input seqfile %s exceeds the upper limit %d Mb." % (seqfile, MAX_FILESIZE_IN_MB),
-                  file=sys.stderr)
-            print("Please split your seqfile and submit again.", file=sys.stderr)
+        if len(str(seqbuddy)) >= MAX_FILESIZE:
+            print("You input seqfile is too large! Please split the file into chunks less than %d Mb."
+                  % MAX_FILESIZE_IN_MB, file=sys.stderr)
             return 1
-        seq = read_file(seqfile)
 
-        fixtop = ""
-        if fixtopfile != "":
-            fixtop = read_file(fixtopfile)
+        # ***** Here's the meat ***** #
         myclient = Client(wsdl_url, cache=None)
-        ret_value = myclient.service.submitjob(seq, fixtop, jobname, email)
+        ret_value = myclient.service.submitjob(str(seqbuddy), fixtop, in_args.jobname, in_args.email)
         if len(ret_value) >= 1:
-            strs = ret_value[0]
-            jobid = strs[0]
-            # result_url = strs[1]
-            numseq_str = strs[2]
-            errinfo = strs[3]
-            warninfo = strs[4]
+            jobid, result_url, numseq_str, errinfo, warninfo = ret_value[0][:5]
             if jobid != "None" and jobid != "":
                 print("You have successfully submitted your job with %s sequences. jobid = %s" % (numseq_str, jobid))
                 if warninfo != "" and warninfo != "None":
@@ -207,38 +55,33 @@ def main(g_params):
             print("Failed to submit job!")
             return 1
     else:
-        if jobid == "":
-            print("You want to get the result of a job but jobid not set. exit!", file=sys.stderr)
-            return 1
         myclient = Client(wsdl_url, cache=None)
-        ret_value = myclient.service.checkjob(jobid)
+        ret_value = myclient.service.checkjob(in_args.input)
         if len(ret_value) >= 1:
-            strs = ret_value[0]
-            status = strs[0]
-            result_url = strs[1]
-            errinfo = strs[2]
+            status, result_url, errinfo = ret_value[0][:3]
+
             if status == "Failed":
-                print("Your job with jobid %s is failed!" % jobid)
+                print("Your job with jobid %s is failed!" % in_args.input)
                 if errinfo != "" and errinfo != "None":
                     print("Error message:\n%s" % errinfo)
             elif status == "Finished":
-                print("Your job with jobid %s is finished!" % jobid)
-                if not os.path.exists(outpath):
+                print("Your job with jobid %s is finished!" % in_args.input)
+                if not os.path.exists(in_args.outpath):
                     try:
-                        os.makedirs(outpath)
+                        os.makedirs(in_args.outpath)
                     except OSError:
-                        print("Failed to create the outpath %s" % outpath)
+                        print("Failed to create the outpath %s" % in_args.outpath)
                         return 1
-                outfile = "%s/%s.zip" % (outpath, jobid)
+                outfile = "%s/%s.zip" % (in_args.outpath, in_args.input)
                 urllib.request.urlretrieve(result_url, outfile)
                 if os.path.exists(outfile):
-                    print("The result file %s has been retrieved for jobid %s" % (outfile, jobid))
+                    print("The result file %s has been retrieved for jobid %s" % (outfile, in_args.input))
                 else:
-                    print("Failed to retrieve result for jobid %s" % jobid)
+                    print("Failed to retrieve result for jobid %s" % in_args.input)
             elif status == "None":
-                print("Your job with jobid %s does not exist! Please check you typing!" % jobid)
+                print("Your job with jobid %s does not exist! Please check you typing!" % in_args.input)
             else:
-                print("Your job with jobid %s is not ready, status = %s" % (jobid, status))
+                print("Your job with jobid %s is not ready, status = %s" % (in_args.input, status))
         else:
             print("Failed to get job!")
             return 1
@@ -246,9 +89,23 @@ def main(g_params):
     return 0
 
 
-def init_global_parameter():
-    return {'isQuiet': True}
+MAX_FILESIZE_IN_MB = 9
+MAX_FILESIZE = MAX_FILESIZE_IN_MB * 1024 * 1024
 
 if __name__ == '__main__':
-    in_args = init_global_parameter()
-    sys.exit(main(in_args))
+    progname = os.path.basename(sys.argv[0])
+    wspace = ''.join([" "] * len(progname))
+
+    import argparse
+
+    parser = argparse.ArgumentParser(prog="topcons2_wsdl.py", description="Access topcons2 through a WSDL/SOAP web "
+                                                                          "service (http://v2.topcons.net)")
+
+    parser.add_argument("input", help="Location of protein input FASTA file or the job id", action="store")
+    parser.add_argument("-o", "--outpath", default=os.getcwd(), action="store",
+                        help="Save the retrieved data to outpath")
+    parser.add_argument("-e", "--email", action="store", default="",
+                        help="Specify an email address if you want a notification when the job is complete")
+    parser.add_argument("-n", "--jobname", action="store", default="", help="Give the job a name")
+
+    sys.exit(main(parser.parse_args()))
