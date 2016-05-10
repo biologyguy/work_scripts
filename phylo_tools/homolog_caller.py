5ca56607356c2bd98953f347e9d5fdcd1b2dd0c1
biologyguy <biologyguy@gmail.com>
2016-05-10 12:30:55 -0400

    Remove support function and start to set up script for MAFFT comparison
    - Probably non-functional at the moment.

--- phylo_tools/homolog_caller.py
+++ phylo_tools/homolog_caller.py
@@ -28,6 +28,8 @@ import scipy.stats
 # My packages
 import mcmcmc  # Note: This is in ../utilities and sym-linked to python3.5/site-packages
 import MyFuncs
+import SeqBuddy
+import AlignBuddy
 
 
 class Clusters:  # The cluster groups should not include anything more than the groups (no names)
@@ -173,8 +175,8 @@ def mcmcmc_mcl(args, params):
     return score
 
 
-def clique_checker(cluster, df_all_by_all):  # ToDo: When pulling in best hits, check for multiple equal best hits
-    def get_best_hit(gene_name):
+def clique_checker(cluster, df_all_by_all):
+    def get_best_hit(gene_name):  # ToDo: When pulling in best hits, check for multiple equal best hits
         _best_hit = df_all_by_all[(df_all_by_all[0] == gene_name) | (df_all_by_all[1] == gene_name)]
         _best_hit.columns = ["subj", "query", "score"]
         _best_hit = _best_hit.loc[_best_hit["score"] == max(_best_hit["score"])].values[0]
@@ -194,6 +196,8 @@ def clique_checker(cluster, df_all_by_all):  # ToDo: When pulling in best hits, 
             for j, rec in taxa.iterrows():
                 _gene = "%s-%s" % (rec["taxa"], rec["gene"])
                 best_hit = get_best_hit(_gene)
+                # Don't know what order the "subj" and "query" genes are in, so check
+                # in_dict[_gene] = (<the best hit gene>, <best hit sim score>)
                 in_dict[_gene] = (best_hit[0], best_hit[2]) if best_hit[1] == _gene else (best_hit[1], best_hit[2])
 
     # Iterate through in_dict and pull in all genes to fill out all best-hit sub-graphs from duplicates
@@ -453,148 +457,16 @@ def merge_singles(clusters, scores):
     clusters += [value for _ind, value in small_clusters.items()]
     return clusters
 
-
-# ToDo: Completely rethink support... MCL is deterministic, so it doesn't make sense to re-run the MCMCMC... Or any of this, really.
-def support(orig_clusters, all_by_all, mode, num_samples, mcmcmc_steps, level=0.632):  # level=0.632
-    def mc_sample_support(jn_sample):
-        samp_all_by_all = split_all_by_all(all_by_all, jn_sample.cluster)["removed"]
-
-        sample_clusters = []
-        sample_clusters = homolog_caller(jn_sample, samp_all_by_all, sample_clusters, 0, False, steps=mcmcmc_steps)
-        sample_clusters = merge_singles(sample_clusters, samp_all_by_all)
-
-        for _orig_clust in orig_clusters:
-            orig_copy = copy(_orig_clust)
-            orig_copy.cluster = [x for x in _orig_clust.cluster if x in jn_sample.cluster]
-            orig_copy.len = len(orig_copy.cluster)
-            if orig_copy.len == 0:
-                continue
-            len_subj = orig_copy.len
-            tally = 0.
-
-            for query in sample_clusters:
-                query.cluster = [x for x in query.cluster if x in jn_sample.cluster]
-                query.len = len(query.cluster)
-                matches = len(set(orig_copy.cluster).intersection(query.cluster))
-                if matches > 0:
-                    len_subj -= matches
-                    weighted_match = ((matches * 2.) / (orig_copy.len + query.len)) ** 1.5
-                    weighted_match *= matches / orig_copy.len
-                    tally += weighted_match
-
-                    # track genes support
-                    for matched_gene in set(orig_copy.cluster).intersection(query.cluster):
-                        row_ind = len(_orig_clust.gene_support[matched_gene])
-                        try:
-                            # Don't count the gene matching itself (i.e., subtract 1 from matches and orig.len)
-                            _orig_clust.gene_support.set_value(row_ind, matched_gene, (matches - 1) / (orig_copy.len - 1))
-                        except ZeroDivisionError:
-                            _orig_clust.gene_support.set_value(row_ind, matched_gene, None)
-
-                    if len_subj == 0:
-                        break
-            if tally == 0:
-                raise ValueError("Why is tally 0??")
-
-            _orig_clust.support = _orig_clust.support.append(pd.Series([tally]))
-
-        _output = ">>>\n"
-        _output += ">>group_support\n"
-        for _orig_clust in orig_clusters:
-            string_io = StringIO()
-            _orig_clust.support.to_csv(string_io)
-            _output += "%s\n%s" % (_orig_clust.name, string_io.getvalue())
-
-        _output += ">>gene_support\n"
-        for _orig_clust in orig_clusters:
-            string_io = StringIO()
-            _orig_clust.gene_support.to_csv(string_io)
-            _output += "%s\n%s" % (_orig_clust.name, string_io.getvalue())
-
-        with lock:
-            temp_file.write(_output)
-
-    total_pop = [x.cluster for x in orig_clusters]  # List of lists
-    total_pop = [x for _clust in total_pop for x in _clust]  # Flatten to a 1D list
-
-    for _clust in orig_clusters:
-        # Add a new 'support' attribute to each Cluster object which will be appended to at each step
-        _clust.support = pd.Series()
-        # Create a dataframe to keep track of support for each individual gene
-        _clust.gene_support = pd.DataFrame(columns=_clust.cluster)
-
-    # If new ways to subsample the total population are dreamed up, create another sample_generator here...
-    if mode == "taxa":
-        taxa_list = []
-        for _gene in total_pop:
-            taxa = _gene.split("-")[0]
-            if taxa not in taxa_list:
-                taxa_list.append(taxa)
-
-        def sample_generator():
-            _samples = []
-            for _taxa in taxa_list:
-                _samples.append(Cluster([x for x in total_pop if x.split("-")[0] != _taxa]))
-            return _samples
-
-    elif mode == "jackknife":
-        sample_size = floor(level * len(total_pop))
-
-        def sample_generator():
-            _samples = []
-            for _ in range(num_samples):
-                _samples.append(Cluster(sample(total_pop, sample_size)))
-            return _samples
-
-    else:
-        raise ValueError("Mode '%s' not supported." % mode)
-
-    temp_file = MyFuncs.TempFile()
-    samples = sample_generator()
-    cpus = ceil(MyFuncs.usable_cpu_count() / 3)
-    MyFuncs.run_multicore_function(samples, mc_sample_support, max_processes=cpus)
-
-    results = temp_file.read().split(">>>\n")[1:]
-    for result in results:
-        group_sup, gene_sup = result.split("\n>>gene_support\n")
-        group_sup = group_sup.strip().strip(">>group_support\n")
-        # Cluster support
-        for group in group_sup.split("group_"):
-            group = group.splitlines()
-            if len(group) == 1:
-                continue
-
-            gsup = group[1].split(",")
-            gsup = pd.Series(float(gsup[1])) if group[1] != "" else pd.Series(None)
-
-            for orig_clust in orig_clusters:
-                if orig_clust.name == "group_%s" % group[0]:
-                    orig_clust.support = pd.concat([orig_clust.support, gsup], ignore_index=True)
-
-        # Gene support
-        for group in gene_sup.split("group_")[1:]:
-            group = group.splitlines()
-            gsup = StringIO("\n".join(group[1:]))
-
-            gsup = pd.DataFrame.from_csv(gsup)
-
-            for orig_clust in orig_clusters:
-                    if orig_clust.name == "group_%s" % group[0]:
-                        orig_clust.gene_support = orig_clust.gene_support.append(gsup, ignore_index=True)
-    return orig_clusters
+    # NOTE: There used to be a support function. Check the GitHub history if there's desire to bring parts of it back
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(prog="homolog_caller", description="",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument("all_by_all_file", help="Location of all-by-all scores file", action="store")
+    parser.add_argument("sequences", help="Location of sequence file", action="store")
     parser.add_argument("output_file", action="store", nargs="?",
                         help="Where should groups be written to? Default to stdout.")
-    parser.add_argument("-jk", "--jackknife", help="Find support for previous run.", metavar="<Groups file>")
-    parser.add_argument("-sm", "--support_mode", choices=["taxa", "jackknife"], default="jackknife",
-                        help="Select the method for calculating support values.")
-    parser.add_argument("-ss", "--support_steps", type=int, default=100, help="How many jackknife replicates?")
     parser.add_argument("-sz", "--sample_size", type=float, default=0.632,
                         help="Proportion of total population to use in each jackknife replicate")
     parser.add_argument("-mcs", "--mcmcmc_steps", default=1000, type=int,
@@ -619,7 +491,13 @@ if __name__ == '__main__':
     best_clusters = None
     lock = Lock()
 
-    counter = 1
+    sequences = SeqBuddy.SeqBuddy(in_args.sequences)
+
+    if in_args.save_temp_files:
+        if not os.path.isdir(in_args.save_temp_files):
+            os.makedirs(in_args.save_temp_files)
+
+    print("Executing Homolog Caller...")
 
     scores_data = pd.read_csv(os.path.abspath(in_args.all_by_all_file), in_args.separator, header=None)
 
@@ -631,69 +509,41 @@ if __name__ == '__main__':
     taxa_count = pd.Series(taxa_count)
     taxa_count = taxa_count.value_counts()
 
-    if in_args.save_temp_files:
-        if not os.path.isdir(in_args.save_temp_files):
-            os.makedirs(in_args.save_temp_files)
+    final_clusters = []
+    final_clusters = homolog_caller(master_cluster, scores_data, final_clusters, 0, save=in_args.save_temp_files,
+                                    global_all_by_all=scores_data, steps=in_args.mcmcmc_steps,
+                                    global_taxa_count=taxa_count, quiet=False, clique_check=clique_check,
+                                    recursion=recursion_check)
 
-    if in_args.jackknife:
-        print("Preparing support values in '%s' mode" % in_args.support_mode)
-        with open(in_args.jackknife, "r") as ifile:
-            groups = ifile.read()
+    output = ""
+    for clust in final_clusters:
+        output += "group_%s\t%s\t" % (clust.name, clust.score())
+        for seq_id in clust.cluster:
+            output += "%s\t" % seq_id
+        output = "%s\n" % output.strip()
 
-        groups = groups.strip().split("\n")
-        final_clusters = [group.strip().split("\t") for group in groups]
-        for i, clust in enumerate(final_clusters):
-            name = clust[0]
-            clust = Cluster(clust[2:])
-            clust.name = name
-            final_clusters[i] = clust
+    # Try to fold singletons and doublets back into groups.
+    if not in_args.supress_singlet_folding:
+        final_clusters = merge_singles(final_clusters, scores_data)
 
-        final_clusters = support(final_clusters, scores_data, in_args.support_mode,
-                                 in_args.support_steps, in_args.mcmcmc_steps, level=in_args.sample_size)
+    # Format the clusters and output to stdout or file
+    output = ""
+    while len(final_clusters) > 0:
+        _max = (0, 0)
+        for ind, clust in enumerate(final_clusters):
+            if len(clust.cluster) > _max[1]:
+                _max = (ind, len(clust.cluster))
 
-        for clust in final_clusters:
-            print("%s %s %s" % (clust.name, clust.support.mean(), clust.support.std()))
-            for gene in clust.cluster:
-                print("%s %s" % (gene, round(clust.gene_support[gene].mean(), 3)))
+        ind, _max = _max[0], final_clusters[_max[0]]
+        del final_clusters[ind]
+        output += "group_%s\t%s\t" % (_max.name, _max.score())
+        for seq_id in _max.cluster:
+            output += "%s\t" % seq_id
+        output = "%s\n" % output.strip()
+
+    if not in_args.output_file:
+        print(output)
 
     else:
-        print("Executing Homolog Caller...")
-
-        final_clusters = []
-        final_clusters = homolog_caller(master_cluster, scores_data, final_clusters, 0, save=in_args.save_temp_files,
-                                        global_all_by_all=scores_data, steps=in_args.mcmcmc_steps,
-                                        global_taxa_count=taxa_count, quiet=False, clique_check=clique_check,
-                                        recursion=recursion_check)
-
-        output = ""
-        for clust in final_clusters:
-            output += "group_%s\t%s\t" % (clust.name, clust.score())
-            for seq_id in clust.cluster:
-                output += "%s\t" % seq_id
-            output = "%s\n" % output.strip()
-
-        # Try to fold singletons and doublets back into groups.
-        if not in_args.supress_singlet_folding:
-            final_clusters = merge_singles(final_clusters, scores_data)
-
-        # Format the clusters and output to stdout or file
-        output = ""
-        while len(final_clusters) > 0:
-            _max = (0, 0)
-            for ind, clust in enumerate(final_clusters):
-                if len(clust.cluster) > _max[1]:
-                    _max = (ind, len(clust.cluster))
-
-            ind, _max = _max[0], final_clusters[_max[0]]
-            del final_clusters[ind]
-            output += "group_%s\t%s\t" % (_max.name, _max.score())
-            for seq_id in _max.cluster:
-                output += "%s\t" % seq_id
-            output = "%s\n" % output.strip()
-
-        if not in_args.output_file:
-            print(output)
-
-        else:
-            with open(in_args.output_file, "w") as ofile:
-                ofile.write(output)
+        with open(in_args.output_file, "w") as ofile:
+            ofile.write(output)
