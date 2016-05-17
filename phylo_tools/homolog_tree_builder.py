092e1a566d048dc6be9403d89b2fe26c014b80b1
biologyguy <biologyguy@gmail.com>
2016-05-17 11:05:19 -0400

    Add color support for my Ctenophore Panx set
    These are being used all the time for testing, so might as well just support them... Saves time in the long run.

--- phylo_tools/homolog_tree_builder.py
+++ phylo_tools/homolog_tree_builder.py
@@ -7,7 +7,8 @@ Convert the output from homolog_caller into an SVG tree of polytomies
 """
 import sys
 
-class Nexus:
+
+class Nexus(object):
     def __init__(self, node_list):
         self.node_list = node_list
 
@@ -60,7 +61,7 @@ class Nexus:
         return length
 
 
-class Node:
+class Node(object):
     def __init__(self, leaf_list, rank, ave_support, std_support):
         self.leaf_list = sorted(leaf_list, key=lambda x: x.support, reverse=True)
         self.rank = rank.split("_")
@@ -71,13 +72,17 @@ class Node:
         return len(self.leaf_list)
 
 
-class Leaf:
-    def __init__(self, label, _support=None):
+class Leaf(object):
+    def __init__(self, label, _support=None, _color=None):
         self.label = label
         if _support == "nan":
             _support = None
         self.support = _support
         self.color = support_color(_support)
+        if _color:
+            _color = [hex_string(rgb) for rgb in _color]
+            red, green, blue = _color
+            self.color = "%s%s%s" % (red, green, blue)
 
 
 def support_color(_support):
@@ -101,14 +106,66 @@ def hex_string(value):
     output = "0x%0.2X" % int(value)
     return output[2:]
 
+
+class CtenoColors(object):
+    def __init__(self):
+        panx1 = ["Bab-PanxαC", "Bch-PanxαD", "Bfo-PanxαC", "Bfr-PanxαB", "BOL-PanxαE", "Cfu-PanxαA", "Cfu-PanxαB",
+                 "Cfu-PanxαD", "Cfu-PanxαF", "Dgl-PanxαF", "Edu-PanxαF", "Hca-PanxαE", "Hru-PanxαD", "Lcr-PanxαC",
+                 "Lla-PanxαA", "Mle-Panxα11", "Oma-PanxαD", "Pba-PanxαB", "Tin-PanxαF", "Vpa-PanxαD"]
+        panx2 = ["Bab-PanxαD", "Bch-PanxαE", "Bfo-PanxαD", "Bfr-PanxαC", "BOL-PanxαC", "Cfu-PanxαC", "Dgl-PanxαG",
+                 "Edu-PanxαB", "Hca-PanxαH", "Hru-PanxαE", "Lcr-PanxαJ", "Lla-PanxαB", "Mle-Panxα12", "Oma-PanxαA",
+                 "Tin-PanxαE", "Vpa-PanxαG"]
+        panx3 = ["Bab-PanxαB", "Bch-PanxαC", "Bfo-PanxαB", "BOL-PanxαA", "Dgl-PanxαE", "Edu-PanxαA", "Hca-PanxαB",
+                 "Hru-PanxαA", "Lcr-PanxαH", "Mle-Panxα10A", "Mle-Panxα9", "Oma-PanxαC", "Tin-PanxαC", "Vpa-PanxαB"]
+        panx4 = ["Bab-PanxαA", "Bch-PanxαA", "Bfo-PanxαE", "Bfr-PanxαA", "BOL-PanxαB", "Dgl-PanxαI", "Edu-PanxαE",
+                 "Hca-PanxαA", "Lcr-PanxαG", "Lla-PanxαC", "Mle-Panxα3", "Pba-PanxαD", "Tin-PanxαD", "Vpa-PanxαE"]
+        panx5 = ["Bab-PanxαE", "Bfo-PanxαI", "BOL-PanxαD", "Dgl-PanxαD", "Hru-PanxαB", "Lcr-PanxαD", "Mle-Panxα2",
+                 "Pba-PanxαG", "Tin-PanxαB"]
+        panx6 = ["Bch-PanxαB", "Bfo-PanxαJ", "BOL-PanxαF", "Hca-PanxαG", "Lcr-PanxαI", "Mle-Panxα4", "Pba-PanxαA",
+                 "Vpa-PanxαA"]
+        panx7 = ["Bfo-PanxαA", "Bfo-PanxαG", "Dgl-PanxαA", "Edu-PanxαD", "Hru-PanxαC", "Lcr-PanxαB", "Mle-Panxα1",
+                 "Oma-PanxαB"]
+        panx8 = ["Bfr-PanxαD", "Edu-PanxαH", "Hca-PanxαC", "Lcr-PanxαK", "Mle-Panxα7A", "Pba-PanxαF"]
+        panx9 = ["BOL-PanxαH", "Dgl-PanxαH", "Edu-PanxαC", "Hca-PanxαF", "Mle-Panxα8", "Pba-PanxαC"]
+        panx10 = ["Edu-PanxαG", "Lcr-PanxαA", "Lcr-PanxαL", "Mle-Panxα5", "Pba-PanxαE", "Vpa-PanxαF"]
+        panx11 = ["Bfo-PanxαH", "Cfu-PanxαE", "Dgl-PanxαB", "Dgl-PanxαC", "Lcr-PanxαE", "Tin-PanxαA"]
+        panx12 = ["Bfo-PanxαF", "Hca-PanxαD", "Mle-Panxα6"]
+        panxβ = ["Hvu-PanxβA", "Hvu-PanxβB", "Hvu-PanxβC", "Hvu-PanxβD", "Hvu-PanxβE", "Hvu-PanxβF", "Hvu-PanxβG",
+                 "Hvu-PanxβH", "Hvu-PanxβI", "Hvu-PanxβJ", "Hvu-PanxβK", "Hvu-PanxβL", "Hvu-PanxβM", "Hvu-PanxβN",
+                 "Hvu-PanxβO"]
+
+        color_map = {"panx1": (247, 116, 2), "panx2": (0, 0, 0), "panx3": (13, 73, 43), "panx4": (153, 102, 51),
+                     "panx5": (103, 47, 143), "panx6": (57, 180, 74), "panx7": (8, 114, 185), "panx8": (236, 28, 36),
+                     "panx9": (167, 0, 233), "panx10": (236, 30, 121), "panx11": (250, 236, 34),
+                     "panx12": (20, 115, 153)}
+
+        self.reverse_map = {}
+        for indx, group in enumerate([panx1, panx2, panx3, panx4, panx5, panx6, panx7,
+                                      panx8, panx9, panx10, panx11, panx12]):
+            for gene in group:
+                self.reverse_map[gene] = color_map["panx%s" % (indx + 1)]
+
+        for gene in panxβ:
+            self.reverse_map[gene] = (32, 61, 255)
+
+    def get_color(self, gene):
+        try:
+            return self.reverse_map[gene]
+        except KeyError:
+            return tuple([0, 0, 0])
+
 if __name__ == '__main__':
     import argparse
 
     parser = argparse.ArgumentParser(prog="homolog_tree_builder", description="",
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument("support", help="", action="store")
+    parser.add_argument("-panx", "--ctenos_panxs", action="store_true", help="Add color to Ctenophore pannexins.")
 
     in_args = parser.parse_args()
+
+    if in_args.ctenos_panxs:
+        ctenos = CtenoColors()
 
     with open(in_args.support, "r") as ifile:
         support_file = ifile.read().strip().split("group_")[1:]
@@ -120,7 +177,10 @@ if __name__ == '__main__':
         support = node[1]
         leaves = []
         for leaf in node[2:]:
-            leaves.append(Leaf(leaf, 1.0))
+            if in_args.ctenos_panxs:
+                leaves.append(Leaf(leaf, 1.0, ctenos.get_color(leaf)))
+            else:
+                leaves.append(Leaf(leaf, 1.0))
 
         nodes.append(Node(leaves, rank=node[0], ave_support=0,
                           std_support=0))
