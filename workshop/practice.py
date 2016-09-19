a5bcb7272ee0de6966d5efe8b890fe04077f1ac1
Alex Jones <alexjones1725@gmail.com>
2016-09-19 16:39:19 -0400

    Converted practice.py into a function (protein_parse)
    I converted the script to a function to allow for the user to specify which data file they would like to use with protein_parse.

--- workshop/practice.py
+++ workshop/practice.py
@@ -2,36 +2,39 @@
 # -*- coding: utf-8 -*-
 # Created on: Sep 19 2016
 
-# input file
-InFileName = "/Users/jonesalm/practice_data/tps1.fa"
 
-# open file in read mode
-InFile = open(InFileName, 'r')
-# create empty dictionary
-SeqDict = {}
+def protein_parse(infile):
 
-for Line in InFile:
-    if Line.startswith('>'):
-        # if the line starts with a '>', strip the line endings
-        Line = Line.strip('\n')
-        # split the line whenever there is a '|'
-        ElementList = Line.split('|')
-        # 4th element in the list is the access number
-        AccessNum = ElementList[3]
-        if AccessNum.startswith("NP"):
-            # create a key in the SeqDict for each access number found
-            SeqDict[AccessNum] = []
-            print(">" + AccessNum)
-    else:
-        if AccessNum.startswith("NP"):
-            # if the line doesnt start with '>', AKA a line of protein sequences, strip the line ending
-            SeqLine = Line.strip('\n')
-            # append this SeqLine to a list of seqs for that AccessNum
-            SeqDict[AccessNum].append(SeqLine)
-            print(SeqLine)
+    # open file in read mode
+    infile = open(infile, 'r')
+    # create empty dictionary
+    seq_dict = {}
 
-DictLength = len(SeqDict)
-print(DictLength)
-print('Number of Sequences: %d' % DictLength)
+    for line in infile:
+        if line.startswith('>'):
+            # if the line starts with a '>', strip the line endings
+            line = line.strip('\n')
+            # split the line whenever there is a '|'
+            element_list = line.split('|')
+            # 4th element in the list is the access number
+            access_num = element_list[3]
+            if access_num.startswith("NP"):
+                # create a key in the seq_dict for each access number found
+                seq_dict[access_num] = []
+                print(">" + access_num)
+        else:
+            if access_num.startswith("NP"):
+                # if the line doesnt start with '>', AKA a line of protein sequences, strip the line ending
+                seq_line = line.strip('\n')
+                # append this seq_line to a list of seqs for that access_num
+                seq_dict[access_num].append(seq_line)
+                print(seq_line)
 
-InFile.close()
+    dict_length = len(seq_dict)
+    print(dict_length)
+    print('Number of Sequences: %d' % dict_length)
+
+    infile.close()
+
+if __name__ == '__main__':
+    protein_parse('/Users/jonesalm/practice_data/tps1.fa')
